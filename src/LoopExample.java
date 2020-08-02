83036cac2d0e59f0c78c86b85fbad7409bf43f67
yadu-cpu <bhopatkar.yadnesh@gmail.com>
2020-08-02 08:46:53 +0530

    Addition java file is added.

---
+++ src/LoopExample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LoopExample {
+	public static void main(String args[])
+	{
+		int a,b;
+		int sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of a");
+		a=sc.nextInt();
+		System.out.println("Enter value of b");
+		b=sc.nextInt();
+		sum=a+b;
+		System.out.println("Sum is");
+		System.out.println(sum);
+		
+		
+
+	}
+
+}
